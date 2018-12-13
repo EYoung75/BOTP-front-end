bea5c098a43475ffd22fd17f9e042ac21955e9e4
s0njablade <sonjita.chacon@gmail.com>
2018-12-13 15:22:45 -0700

    created trans component, fetch requests, passed props

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 import Router from "./components/router.jsx";
 
 class App extends Component {
+
   render() {
+
+
     return (
       <div className="App">
         <Router />
--- src/components/router.jsx
+++ src/components/router.jsx
@@ -2,16 +2,51 @@ import React from "react"
 import { BrowserRouter, Route } from "react-router-dom"
 import Login from "./login.jsx";
 import Account from "./account.jsx";
+import Transactions from "./transactions.jsx";
 
-const Router = () => {
-    return (
-        <BrowserRouter>
-            <div>
-                <Route path="/" exact component={Login}></Route>
-                <Route path="/account" component={() => <Account></Account>}></Route>
-            </div>
-        </BrowserRouter>
-    )
+class Router extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            users: [],
+            accounts: [],
+            user: "",
+            password: ""
+        }
+    }
+
+    async componentDidMount() {
+        const userResponse = await fetch('https://peoples-db.herokuapp.com/users')
+        const userJson = await userResponse.json()
+        const accountsResponse = await fetch('https://peoples-db.herokuapp.com/accounts')
+        const accountsJson = await accountsResponse.json()
+        this.setState({
+            users: userJson,
+            accounts: accountsJson
+        })
+    }
+
+    handleInput = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+        console.log(this.state.user)
+    }
+
+    render() {
+
+        console.log(this.state.accounts)
+
+        return (
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact render={() => <Login handleInput={this.handleInput}></Login>}></Route>
+                    <Route path="/account" component={() => <Account users={this.state.users} accounts={this.state.accounts}></Account>}></Route>
+                    <Route path="/transactions" component={() => <Transactions></Transactions>}></Route>
+                </div>
+            </BrowserRouter>
+        )
+    }
 }
 
 export default Router
